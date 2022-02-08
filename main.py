e42a648f8a6951c6795f4e21d63bb1044fdb7ee4
butlermathandscienceclub <80225040+butlermathandscienceclub@users.noreply.github.com>
2022-02-08 18:36:05 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,32 @@
 from flask import *
 from search import *
-from time import perf_counter
 app = Flask('app')
+p=True
 @app.route('/', methods=['GET', 'POST'])
 def main():
 	s = request.args.get("search") ;
-	start = perf_counter()
 	r = get_results(s)
-	end = perf_counter()
-	result_time = str(end-start)
-	s = "found results in {} seconds <br>".format(result_time)
+	style = """
+	text-align:center;
+	"""
+	s = f"<div style=\"{style}\"><h1>results</h1><br>"
+	print(r)
 	for i in r:
 		s += (f"<a href=\"{i}\" target=\"_blank\" >{i}</a><br>")
-		
-	return f'{s if r !="" else f"no results for your search " }'
+	s+="</div>"
+	l = """<div style="
+	text-align:center;
+	"><h1>results</h1><br></div>"""
+	return f'{s if s != l else "no results"}'
 @app.route('/new', methods=['GET', 'POST'])
 def r2():
-	s = eval(request.args.get("kwdlist")) 
-	url = request.args.get("url")
-	x=new2(url, *s)
-	return f'{x} '
+	if not p:
+		s = eval(request.args.get("kwdlist")) 
+		url = request.args.get("url")
+		x=new2(url, *s)
+		return f'{x} '
+
+	
 
 
 app.run(host='0.0.0.0', port=8080)
