0f09a83ec6ea08e711f91ec407521728e466260c
butlermathandscienceclub <80225040+butlermathandscienceclub@users.noreply.github.com>
2022-02-11 17:36:18 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,12 +4,16 @@ app = Flask('app')
 p=True
 @app.route('/', methods=['GET', 'POST'])
 def main():
+	d= request.args.get("dark")
 	s = request.args.get("search") ;
 	r = get_results(s)
-	style = """
+	style= """
 	text-align:center;
 	"""
-	s = f"<div style=\"{style}\"><h1>results</h1><br>"
+	s1 = "	<style>:root{color:black}</style>"
+	s2 = "	<style>:root{color:white; background-color:black;}</style>"
+	z = s2 if d == "2" else s1
+	s = f"{z}<div style=\"{style}\"><h1>results</h1><br>"
 	print(r)
 	for i in r:
 		s += (f"<a href=\"{i}\" target=\"_blank\" >{i}</a><br>")
@@ -17,7 +21,10 @@ def main():
 	l = """<div style="
 	text-align:center;
 	"><h1>results</h1><br></div>"""
+	res = f'{s if s != l else "no results"}'
+	print(res)
 	return f'{s if s != l else "no results"}'
+
 @app.route('/new', methods=['GET', 'POST'])
 def r2():
 	if not p:
